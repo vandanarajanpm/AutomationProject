e4bbba8ed0f2185eb66b6a5a7252258fc27b0b67
sives <sives@192.168.70.52>
2025-11-01 11:28:36 +0400

    fifth commit

---
+++ AutomationCourse/src/test/java/seleniumBasics/Base.java
@@ -0,0 +1,26 @@
+package seleniumBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+public WebDriver driver;
+public void initializeBrowser() {
+	driver=new ChromeDriver();
+	driver.get("https://selenium.qabible.in/");
+	driver.manage().window().maximize();
+}
+public void browserCloseAndQuit() {
+	//driver.close();
+	driver.quit();
+}
+	public static void main(String[] args) {
+		Base base = new Base();
+		base.initializeBrowser();
+		base.browserCloseAndQuit();
+		
+
+
+	}
+
+}
