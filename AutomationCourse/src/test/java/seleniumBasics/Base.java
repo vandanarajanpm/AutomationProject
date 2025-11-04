4a5708aaa8586edc4c406bb47427071b87f7668b
sives <sives@192.168.70.52>
2025-11-04 13:15:33 +0400

    This is the second program  in  repo

--- AutomationCourse/src/test/java/seleniumBasics/Base.java
+++ AutomationCourse/src/test/java/seleniumBasics/Base.java
@@ -5,15 +5,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Base {
 public WebDriver driver;
+
 public void initializeBrowser() {
 	driver=new ChromeDriver();
 	driver.get("https://selenium.qabible.in/");
 	driver.manage().window().maximize();
 }
+
 public void browserCloseAndQuit() {
 	//driver.close();
 	driver.quit();
 }
+
 	public static void main(String[] args) {
 		Base base = new Base();
 		base.initializeBrowser();
---
+++ AutomationCourse/src/test/java/seleniumBasics/BrowserAndNavigationCommand.java
@@ -0,0 +1,37 @@
+package seleniumBasics;
+
+public class BrowserAndNavigationCommand extends Base{
+	public void browserCommand() {
+		
+		String title=driver.getTitle();
+		System.out.println(title);
+		
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+		
+		String id =driver.getWindowHandle();
+		System.out.println(id);
+		
+		String source=driver.getPageSource();
+		System.out.println(source);
+	}
+	
+	public void navigationCommand() {
+		driver.navigate().to("https://www.amazon.in/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+		
+		
+	}
+
+	public static void main(String[] args) {
+		BrowserAndNavigationCommand cmds = new BrowserAndNavigationCommand();
+		cmds.initializeBrowser();
+		//cmds.browserCommand();
+		cmds.navigationCommand();
+		
+
+	}
+
+}
