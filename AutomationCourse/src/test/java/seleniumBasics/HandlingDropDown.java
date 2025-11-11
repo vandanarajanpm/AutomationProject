57968d4012fd4a88ce192cd20826c6958a856402
sives <sives@192.168.70.52>
2025-11-11 14:17:39 +0400

    commit

---
+++ AutomationCourse/src/test/java/seleniumBasics/HandlingDropDown.java
@@ -0,0 +1,40 @@
+package seleniumBasics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class HandlingDropDown extends Base{
+	public void verifyDropDown() {
+	driver.navigate().to("https://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");	 
+	WebElement dropdown1=driver.findElement(By.id("dropdowm-menu-1"));
+	Select select = new Select(dropdown1);
+	//select.selectByIndex(1);
+	//select.selectByValue("python");
+	select.selectByVisibleText("SQL");
+	}
+	public void verifyCheckBox() {
+		driver.navigate().to("https://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+		WebElement checkBox=driver.findElement(By.xpath("//input[@value='option-2']"));
+		checkBox.click();
+		
+	}
+	public void verifyRadioButton() {
+	driver.navigate().to("https://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+	WebElement radioButton=driver.findElement(By.xpath("//input[@value='green']"));
+	radioButton.click();
+		
+	}
+
+	public static void main(String[] args) {
+		HandlingDropDown handlingdropdown = new HandlingDropDown();
+		handlingdropdown.initializeBrowser();
+		//handlingdropdown.verifyDropDown();
+		//handlingdropdown.verifyCheckBox();
+	      handlingdropdown.verifyRadioButton();
+			
+		}
+
+	}
+
+
--- AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
@@ -1,16 +1,33 @@
 package seleniumBasics;
 
+import java.security.MessageDigest;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class WebElementCommands extends Base {
+	
 	public void verifyWebElementCommands() {
+		
 	driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+	
 	WebElement textbox=driver.findElement(By.id("single-input-field"));
 	textbox.sendKeys("vandana");
 	
 	WebElement showmessagebutton=driver.findElement(By.id("button-one"));
 	showmessagebutton.click();
+	
+	WebElement text=driver.findElement(By.id("message-one"));
+	System.out.println(text.getText());
+	
+	System.out.println(text.getTagName());
+	
+	
+	textbox.clear();
+	
+	System.out.println(showmessagebutton.getCssValue("background-color"));
+	
+	
 	
 	
 		
