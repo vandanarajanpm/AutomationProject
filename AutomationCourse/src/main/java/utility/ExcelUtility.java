83618248ef25ec84f707e392617e749cadad48bd
sives <sives@192.168.70.52>
2025-11-24 10:22:32 +0400

    commit twelve

---
+++ AutomationCourse/src/main/java/utility/ExcelUtility.java
@@ -0,0 +1,34 @@
+package utility;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtility {
+	static FileInputStream f; //excel file 
+	static XSSFWorkbook w; //Book1
+	static XSSFSheet s;// sheets
+	public static String getStringData(int a,int b) throws IOException // a is row ,b is column or cell
+	{
+		f=new FileInputStream("C:\\Users\\sives\\OneDrive\\Desktop\\Book1.xlsx");
+		w=new XSSFWorkbook(f);
+		s=w.getSheet("Sheet1");
+		XSSFRow r=s.getRow(a);
+		XSSFCell c=r.getCell(b);
+		return c.getStringCellValue(); //inbuilt method for excelsheet connection
+	}
+	public static String getIntegerData(int a,int b) throws IOException { 
+		f=new FileInputStream("C:\\Users\\sives\\OneDrive\\Desktop\\Book1.xlsx");
+		w=new XSSFWorkbook(f);
+		s=w.getSheet("Sheet1");
+		XSSFRow r=s.getRow(a);
+		XSSFCell c=r.getCell(b);
+		int y=(int) c.getNumericCellValue();// string - integer  = typecasting
+		return String.valueOf(y);
+	}
+
+}
